ba4ceaeff1cbfe2447b64967d4a30ddfd7e2e426
Zack Grossbart <zack@grossbart.com>
2011-12-27 08:49:46 -0500

    This version has the full working test case.

---
+++ README.md
@@ -0,0 +1,4 @@
+Paper.js multiple canvas test case
+==================================================
+
+Try it out: [zgrossbart.github.com/multipaper](zgrossbart.github.com/multipaper)
---
+++ loader.js
@@ -0,0 +1,21 @@
+loader = {
+};
+
+function loadDrawing(/*string*/ url, /*string*/ id) {
+    jQuery.ajax({
+        url: url + '.pjs',
+        dataType: 'html',
+        success: function(data) {
+            paper.setup(document.getElementById(id));
+            paper.evaluate(data);
+            
+            loader[url].layout();
+        }
+    });
+}
+
+
+$(document).ready(function() {
+    loadDrawing('drawing1', 'canvas1');
+    loadDrawing('drawing2', 'canvas2');
+});
